d183c063ceffb742aba70962334b296f3db1db88
Ermili mohamed <mohamedermili5@gmail.com>
2024-02-12 19:32:35 +0000

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-let nb = process.argv[2];
-
-if(isNaN(parseInt(nb)) ){
-    console.log("Not a number");
-}else{
-    console.log("My number: " + parseInt(nb));
-}
--- 0x12-javascript-warm_up/8-square.js
+++
@@ -1,9 +0,0 @@
-const nbOfOccurrences = process.argv[2];
-let char = "X"
-if(isNaN(parseInt(nbOfOccurrences))){
-    console.log("Missing size");
-}else{
-    for (let i = 0; i<nbOfOccurrences; i++) {
-        console.log(char.repeat(nbOfOccurrences));
-    }
-}
